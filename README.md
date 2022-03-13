d01785270d7963e915050e85befb2c4abf338a9b
Ana Jessica <anajessica15022002.com>
2022-03-13 16:06:42 +0530

    audio not displayed

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ https://towardsdatascience.com/listen-to-pdf-file-using-python-text-to-speech-li
 https://askubuntu.com/questions/699502/ffmpeg-command-not-found
 
 https://stackoverflow.com/questions/65274843/oserror-libespeak-so-1-cannot-open-shared-object-file-no-such-file-or-directo
+
+
+# todo
+
+- M / F voice
+- Language switch
+- Downloadable
+- Dockerize
+- search audios
--- app.py
+++ app.py
@@ -7,63 +7,139 @@ import PyPDF2
 # from pymongo import MongoClient
 
 
-UPLOAD_PATH = "static/"
+UPLOAD_PATH = "pdf/"
+UPLOADED_PATH = "audio/"
 
 app = Flask(__name__)
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_PATH
+app.config['UPLOADED_FOLDER'] = UPLOADED_PATH
 
-@app.route('/')
-def index():
-    return render_template('upload.html')
-
-@app.route('/', methods=['POST'])
-def upload_files():
-    uploaded_file = request.files['file']
-    global filename
-    filename = secure_filename(uploaded_file.filename)
-    if filename != '':
-        file_ext = os.path.splitext(filename)[1]
-        uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-
-    return '', 204
-
+@app.route('/', methods = ['GET', 'POST'])
 def create_audio():
-
-    pdf_file = open('sample.pdf', 'rb')
-    read_pdf = PyPDF2.PdfFileReader(pdf_file, strict=False)
- 
-    number_of_pages = read_pdf.getNumPages()
-
-    engine = pyttsx3.init()
-
-    for i in range(0, number_of_pages ):
- 
-        page = read_pdf.getPage(i)
-        
-        page_content = page.extractText()
-
-        print(page_content)
     
-        newrate=120
-        engine.setProperty('rate', newrate)
-        newvolume=3
-        engine.setProperty('volume', newvolume)
-            
-        # engine.say(page_content) 
+    if request.method == 'POST':
+        
+        uploaded_file    = request.files['file']
 
-        engine.save_to_file(page_content,os.path.join(app.config['UPLOAD_FOLDER'], filename+'.mp3')) 
+        global filename
+        filename = secure_filename(uploaded_file.filename)
+        if filename != '':
+            file_ext = os.path.splitext(filename)[1]
+            uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        global LOCAL_FILE_PATH
+        LOCAL_FILE_PATH = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+        pdf_file = open(UPLOAD_PATH+filename, 'rb')
+        read_pdf = PyPDF2.PdfFileReader(pdf_file, strict=False)
+    
+        number_of_pages = read_pdf.getNumPages()
+
+        # To access voice property of the speaker:
+        # voices = speaker.getProperty('voices')
+        # # Set the speaker's gender: 0-> Male (default), 1-> Female
+        # speaker.setProperty('voice', voices[1].id)
+
+        engine = pyttsx3.init()
+
+        for i in range(0, number_of_pages ):
+    
+            page = read_pdf.getPage(i)
+            
+            page_content = page.extractText()
+
+            print(page_content)
+        
+            newrate=120
+            engine.setProperty('rate', newrate)
+            newvolume=3
+            engine.setProperty('volume', newvolume)
+                
+            # engine.say(page_content) 
+        file_ext = os.path.splitext(filename)[0]
+        audio = os.path.join(app.config['UPLOADED_FOLDER'],file_ext+'.mp3') 
+
+        engine.save_to_file(page_content,audio) 
         
         engine.runAndWait()
 
-    return filename
+        return file_ext
+
+    return render_template('upload.html')
+
+# @app.route('/')
+# def index():
+#     return render_template('upload.html')
+
+# @app.route('/', methods=['POST'])
+# def upload_files():
+#     uploaded_file = request.files['file']
+#     global filename
+#     filename = secure_filename(uploaded_file.filename)
+#     if filename != '':
+
+#         uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#         return filename
 
 
-@app.route('/play', methods = ["GET","POST"])
-def play():
+#     return '', 204
+
+# @app.route('/submit', methods = ["POST"])
+# def create_audio():
+#     filename = upload_files()
+
+#     pdf_file = open(UPLOAD_PATH+filename, 'rb')
+#     read_pdf = PyPDF2.PdfFileReader(pdf_file, strict=False)
  
-    return render_template('play.html',filename = filename)
+#     number_of_pages = read_pdf.getNumPages()
 
+#     engine = pyttsx3.init()
+
+#     for i in range(0, number_of_pages ):
+ 
+#         page = read_pdf.getPage(i)
+        
+#         page_content = page.extractText()
+
+#         print(page_content)
+    
+#         newrate=120
+#         engine.setProperty('rate', newrate)
+#         newvolume=3
+#         engine.setProperty('volume', newvolume)
+            
+#         # engine.say(page_content) 
+#         file_ext = os.path.splitext(filename)[0]
+#         audio = os.path.join(app.config['UPLOAD_FOLDER'], filename+'.mp3')
+
+#         engine.save_to_file(page_content,audio) 
+        
+#         engine.runAndWait()
+
+#         print(audio)
+
+#     return audio
+
+@app.route("/view", methods=["GET","POST"])
+def view():
+    path = '/home/ana/ana/pdf-audio-converter/audio'
+
+    files = os.listdir(path)
+    data =[]
+    
+    for f in files:
+        data.append(f)
+
+    return render_template('view.html', result = data)
+
+@app.route('/single', methods=["GET","POST"])
+def display():
+
+    audio_file = str(request.args['com_nam'])
+ 
+
+    return render_template('play.html', filename = audio_file)
 
 
 
